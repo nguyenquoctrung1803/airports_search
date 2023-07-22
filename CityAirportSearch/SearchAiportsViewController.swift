71aee4cbc75fad3ab0663859dbee7167ed7190a6
Trung Nguyễn Quốc <nguyenquoctrung579@gmail.com>
2023-07-22 11:52:21 +0700

    First commit

---
+++ CityAirportSearch/SearchAiportsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CityAirportSearch
+//
+//  Created by Trung Nguyễn Quốc on 22/07/2023.
+//
+
+import UIKit
+
+class SearchAiportsViewController: UIViewController {
+
+    //MARK: - Outlet
+    @IBOutlet weak var roundedView: UIView!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
